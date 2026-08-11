80ca705880427d0649255334e8f8cdea4e480c8c
baggytsk <127945095+baggytsk@users.noreply.github.com>
2026-08-11 23:38:03 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String s) {
+        int center = s.length() / 2;
+        return (s.length() % 2 == 1) ? s.substring(center, center+1) : s.substring(center-1, center+1);
+        
+    }
+}
