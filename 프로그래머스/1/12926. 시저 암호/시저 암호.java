e6fc78e4f5c44921193d5dd5750b8268d180244a
baggytsk <127945095+baggytsk@users.noreply.github.com>
2026-09-13 19:06:41 +0900

    [level 1] Title: 시저 암호, Time: 0.51 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray()){
+            if(c == ' ') sb.append(c);
+            else if(c-'A' < 26){
+                sb.append((char)('A'+ (c-'A'+n)%26 ));
+            } else{
+                sb.append((char)('a'+ (c-'a'+n)%26 ));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
