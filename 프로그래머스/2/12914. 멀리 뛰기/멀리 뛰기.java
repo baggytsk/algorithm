3e908a3ec766be03ca331dbb8c10f79e6d8d0da9
baggytsk <127945095+baggytsk@users.noreply.github.com>
2026-09-23 17:41:48 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        long[] count = new long[n + 1];
+        count[1] = 1; 
+        count[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            count[i] = (count[i-1] + count[i-2])%1234567;
+        }
+        
+        return count[n];
+    }
+}
